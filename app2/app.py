765617809987b3587a9e68c1a16f085ddda5eb92
Rohit <rohit.truesdale@gmail.com>
2025-02-17 03:26:02 -0600

    micro Service application deployable

---
+++ app2/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+redis_client = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+
+@app.route("/")
+def show_message():
+    message = redis_client.get("latest_message")
+    if message:
+        return f"Last message submitted: {message}"
+    else:
+        return "No messages submitted yet!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005)
