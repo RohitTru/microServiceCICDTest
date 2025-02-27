293c6ed53ef4e33689299ee31a725c01c6fecc4a
Rohit <rohit.truesdale@gmail.com>
2025-02-26 19:46:06 -0600

    pre-commit and linting checks on local side of development

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r app-template/requirements.txt
+
+# Install and activate pre-commit hooks
+pre-commit install
+echo "✅ Pre-commit hooks installed!"
+#!/bin/bash
+
+# Install dependencies
+pip install -r app-template/requirements.txt
+
+# Install and activate pre-commit hooks
+pre-commit install
+echo "✅ Pre-commit hooks installed!"
